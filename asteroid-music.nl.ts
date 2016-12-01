2e565bd7da2489663a5e1b06f63c12cfdf465b23
Sylvia van Os <iamsylvie@openmailbox.org>
2016-12-01 16:12:00 +0100

    Add Dutch translation

---
+++ asteroid-music.nl.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="135"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation>&lt;h3&gt;Geen data&lt;/h3&gt;
+Synchroniseer AsteroidOS met je telefoon.</translation>
+    </message>
+</context>
+</TS>
