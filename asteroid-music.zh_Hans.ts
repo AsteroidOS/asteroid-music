9f8fe46b0bcbf1168161b54d7b96132778a9d5b0
徐杨（Seven小杨） <xy19009188@gmail.com>
2016-12-08 07:17:33 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (1 of 1 strings)

--- asteroid-music.zh_Hans.ts
+++ asteroid-music.zh_Hans.ts
@@ -7,7 +7,8 @@
         <location filename="main.qml" line="135"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt; 1无数据&lt;/h3&gt; 2
+和手机同步中.</translation>
     </message>
 </context>
 </TS>
