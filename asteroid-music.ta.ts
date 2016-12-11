aa72058dc1d98782b7c1599865abf62ca6d32fb9
Khaleel Jageer <jskcse4@gmail.com>
2016-12-11 12:50:58 +0000

    Added translation using Weblate (Tamil)

---
+++ asteroid-music.ta.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ta">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="135"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
