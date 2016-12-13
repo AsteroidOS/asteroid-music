f3d4fd660bc3c5ad39e1400b567f86dcf3489f59
dr_gogeta86 <dr_gogeta86@messinalug.org>
2016-12-13 08:52:23 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (1 of 1 strings)

--- asteroid-music.it.ts
+++ asteroid-music.it.ts
@@ -7,7 +7,8 @@
         <location filename="main.qml" line="135"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Nessun dato&lt;/h3&gt;
+Sincronizza AsteroidOS con il telefono.</translation>
     </message>
 </context>
 </TS>
