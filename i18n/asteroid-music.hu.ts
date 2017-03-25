7e74d57e2389f6a0f97a8bd875ff170d12e765df
Ede Kertesz <edehunor@gmail.com>
2017-03-25 18:13:03 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.hu.ts
+++ i18n/asteroid-music.hu.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-music.desktop.h" line="5"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Zenék</translation>
     </message>
 </context>
 <context>
