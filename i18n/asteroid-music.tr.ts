9c85c59ab9bfa6eaf2392609ae61751b4bfba795
E T <etcetin@gmail.com>
2017-04-18 08:25:36 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.tr.ts
+++ i18n/asteroid-music.tr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-music.desktop.h" line="5"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Müzik</translation>
     </message>
 </context>
 <context>
@@ -15,8 +15,8 @@
         <location filename="../main.qml" line="147"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;Veri Yok&lt;/h3&gt;
-AsteroidOS&apos;u telefonunuzla senkronize edin.</translation>
+        <translation>&lt;h3&gt;Veri yok&lt;/h3&gt;
+AsteroidOS'u telefonunuzla eşzamanlayın.</translation>
     </message>
 </context>
 </TS>
