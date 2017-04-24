a6928dbc3734a7a928d90ae41d377811362af75d
Marc Celeiro Escribà <maceleiro@gmail.com>
2017-04-24 18:38:20 +0000

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.ca.ts
+++ i18n/asteroid-music.ca.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-music.desktop.h" line="5"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Música</translation>
     </message>
 </context>
 <context>
