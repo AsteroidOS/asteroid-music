af36eb592157c022dcdf786040da0a461f00c61e
Laust Lund Kristensen <laustijapan@hotmail.com>
2017-06-21 08:43:59 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.da.ts
+++ i18n/asteroid-music.da.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-music.desktop.h" line="5"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Musik</translation>
     </message>
 </context>
 <context>
@@ -15,7 +15,8 @@
         <location filename="../main.qml" line="149"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Ingen data&lt;/h3&gt;
+Synkroniser AsteroidOS med din telefon.</translation>
     </message>
 </context>
 </TS>
