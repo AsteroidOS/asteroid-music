96cc48060ace42be6fd03493092ce05ea9707ac1
Florent Revest <revestflo@gmail.com>
2017-09-23 07:59:53 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ i18n/asteroid-music.nb.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-music.desktop.h" line="5"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="149"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
