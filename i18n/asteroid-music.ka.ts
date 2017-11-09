139496c1d84a825e464cb16d44b4ea7da47e2be5
sam bowlby <sam@chven.us>
2017-11-09 06:45:07 +0000

    Added translation using Weblate (Georgian)

---
+++ i18n/asteroid-music.ka.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-music.desktop.h" line="5"/>
+        <source>Music</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="149"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
