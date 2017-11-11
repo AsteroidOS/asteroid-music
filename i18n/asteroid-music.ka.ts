baf05ed65df0d0e1935a183b78830c7970b84933
sam bowlby <sam@chven.us>
2017-11-11 15:26:16 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.ka.ts
+++ i18n/asteroid-music.ka.ts
@@ -15,7 +15,8 @@
         <location filename="../main.qml" line="149"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation></translation>
+        <translation>&lt;h3&gt;მონაცემები არ არის&lt;/h3&gt;
+სინქრონიზაცია AsteroidOS თქვენი ტელეფონით.</translation>
     </message>
 </context>
 </TS>
