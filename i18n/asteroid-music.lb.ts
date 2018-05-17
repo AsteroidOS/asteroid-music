7dafdf2ca29d03d4da4f578e0fa17e033c7c802e
Mika Beckerich <beckerichmika@gmail.com>
2018-05-17 21:44:05 +0000

    Added translation using Weblate (Luxembourgish)

---
+++ i18n/asteroid-music.lb.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lb">
+<context>
+    <name></name>
+    <message id="id-no-data-sync">
+        <location filename="../main.qml" line="37"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
+        <translation></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-music.desktop.h" line="6"/>
+        <source>Music</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
