64cb472948c2d69a4464ab785822bd3abd1aa2b7
Mika Beckerich <beckerichmika@gmail.com>
2018-05-25 14:30:53 +0000

    Translated using Weblate (Luxembourgish)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.lb.ts
+++ i18n/asteroid-music.lb.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="37"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation></translation>
+        <translation>&lt;h3&gt;Keng Daten&lt;/h3&gt;Synchroniséier AsteroidOS mat dengem Handy.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation></translation>
+        <translation>Musek</translation>
     </message>
 </context>
 </TS>
