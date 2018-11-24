78e4840b326b541f8fcaf8769a6361a987edc184
ditokp <ditokpl@gmail.com>
2018-11-24 10:59:33 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.id.ts
+++ i18n/asteroid-music.id.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="37"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Tak ada data&lt;/h3&gt; Sinkronisasi AsteroidOS dengan telepon Anda.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Musik</translation>
     </message>
 </context>
 </TS>
