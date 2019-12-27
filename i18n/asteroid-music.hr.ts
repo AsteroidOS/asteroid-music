246922883180f4e656418b2d321486572e5d19c5
Milo Ivir <mail@milotype.de>
2019-12-27 14:55:51 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.hr.ts
+++ i18n/asteroid-music.hr.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="37"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;Nema podataka&lt;/h3&gt;Sinkronizirajte AsteroidOS s mobilnim uređajem.</translation>
+        <translation>&lt;h3&gt;Nema podataka&lt;/h3&gt;Sinkroniziraj AsteroidOS s mobilnim uređajem.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation>Glazba</translation>
+        <translation>Muzika</translation>
     </message>
 </context>
 </TS>
