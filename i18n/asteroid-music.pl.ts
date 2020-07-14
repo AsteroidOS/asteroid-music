ee1497c1c469878487b79839878ef104ae99a26f
kakiremora <piotrek.pastuszak2003@gmail.com>
2020-07-14 18:00:47 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.pl.ts
+++ i18n/asteroid-music.pl.ts
@@ -6,7 +6,7 @@
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="37"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;Brak danych&lt;/h3&gt;Zsynchronizuj AsteroidOS  z telefonem.</translation>
+        <translation>&lt;h3&gt;Brak danych&lt;/h3&gt;Zsynchronizuj AsteroidOS z telefonem.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
