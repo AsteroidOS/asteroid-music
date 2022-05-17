3ec85f64264b08f5ebdfc865b1fbf116bed797a3
phlostically <phlostically@mailinator.com>
2022-05-17 23:47:35 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.eo.ts
+++ i18n/asteroid-music.eo.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="50"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Neniu dateno&lt;/h3&gt;Sinkronigu AsteroidOS kun via telefono.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Muziko</translation>
     </message>
 </context>
 </TS>
