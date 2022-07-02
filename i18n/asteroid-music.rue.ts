266bd9d76033277f356e1e68376c95f4ee8c8cb2
Vondrosh Tenkač <shorksmesharik@gmail.com>
2022-07-02 07:17:50 +0000

    Translated using Weblate (Rusyn)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.rue.ts
+++ i18n/asteroid-music.rue.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="50"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Неє даных&lt;/h3&gt;Сінхронізіруйте AsteroidOS из свойим смартфоном.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Музыкá</translation>
     </message>
 </context>
 </TS>
