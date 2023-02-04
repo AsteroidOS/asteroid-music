675ff983ed6a2fd4327e3ec77edaa3f33ff3f3c0
ⵣⵓⵀⵉⵔ ⴰⵎⴰⵣⵉⵖ ZOUHIR DEHBI <zouhirdehbi56@gmail.com>
2023-02-04 22:29:53 +0100

    Added translation using Weblate (Tamazight (Standard Moroccan))

---
+++ i18n/asteroid-music.zgh.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zgh">
+<context>
+    <name></name>
+    <message id="id-no-data-sync">
+        <location filename="../src/main.qml" line="50"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-music.desktop.h" line="6"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
