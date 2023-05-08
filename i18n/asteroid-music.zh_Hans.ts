511eef22fed47cb8f687b50a4637b1a751ea688b
Integral <integral@murena.io>
2023-05-08 04:58:29 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.zh_Hans.ts
+++ i18n/asteroid-music.zh_Hans.ts
@@ -11,7 +11,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation>音樂</translation>
+        <translation>音乐</translation>
     </message>
 </context>
 </TS>
