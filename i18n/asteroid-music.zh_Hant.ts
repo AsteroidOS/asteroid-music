80185f648658d37b6d7946d2208acf44fa43bfb1
hugoalh <hugoalh@users.noreply.hosted.weblate.org>
2024-05-14 09:42:35 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.zh_Hant.ts
+++ i18n/asteroid-music.zh_Hant.ts
@@ -6,7 +6,7 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="50"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;無資料&lt;/h3&gt;請透過您的手機同步。</translation>
+        <translation>&lt;h3&gt;沒有資料&lt;/h3&gt;將 AsteroidOS 與你的手機同步。</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
