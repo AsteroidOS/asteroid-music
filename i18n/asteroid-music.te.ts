5989ff76fe20601776ff8df1dc095da538068641
Govind Gopal Yadav <govindgopalgaming@gmail.com>
2025-04-26 21:25:06 +0200

    Translated using Weblate (Telugu)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.te.ts
+++ i18n/asteroid-music.te.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="50"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished">&lt;h3&gt;డేటా లేదు&lt;/h3&gt;మీ ఫోన్‌తో AsteroidOSని సమకాలీకరించండి</translation>
+        <translation>&lt;h3&gt;డేటా లేదు&lt;/h3&gt;మీ ఫోన్‌తో AsteroidOSని సమకాలీకరించండి.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation type="unfinished">సంగీతం</translation>
+        <translation>సంగీతం</translation>
     </message>
 </context>
 </TS>
