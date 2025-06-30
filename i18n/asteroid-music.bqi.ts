2e150d556bf748f58fb2392890f75a4db4fb1ad6
hosêyň abāspanā <hosseinabaspanah@gmail.com>
2025-06-30 06:37:37 +0200

    Translated using Weblate (Luri (Bakhtiari))
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.bqi.ts
+++ i18n/asteroid-music.bqi.ts
@@ -6,12 +6,12 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="50"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;بؽ داده&lt;/h3&gt;استروید ن وا تیلیفووݩ تووݩ هوم گوم کۊنین.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>آرنگ</translation>
     </message>
 </context>
 </TS>
