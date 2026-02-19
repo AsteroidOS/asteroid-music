20c30124f68020c8921f9b6aa0aeb8982339c465
Emin Tufan Çetin <etcetin@gmail.com>
2026-02-19 08:49:02 +0100

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-music.tr.ts
+++ i18n/asteroid-music.tr.ts
@@ -6,7 +6,7 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="50"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;Veri yok&lt;/h3&gt; AsteroidOS&apos;u telefonunuzla eşitleyin.</translation>
+        <translation>&lt;h3&gt;Veri yok&lt;/h3&gt;AsteroidOS&apos;u telefonunuzla eşitleyin.</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-music.desktop.h" line="6"/>
